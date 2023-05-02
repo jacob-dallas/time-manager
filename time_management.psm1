632af834556b13a2d2936329cfe2f16dcb83f88c
Pavelka <jacob.pavelka@dallascityhall.com>
2023-05-02 15:51:59 -0500

    fixed folder issue

--- time_management.psm1
+++ time_management.psm1
@@ -4,6 +4,6 @@ function Initialize-Log {
     )
     sqlite3 "task-log.db"
     $local_path = "C:\Users\jacob.pavelka\Documents\PowerShell\Modules"
-    $python_path = $local_path + "\time\.venv\python.exe"
+    $python_path = $local_path + "\time_management\.venv\python.exe"
     $python_path -m test.py "task-log.db"
 }
