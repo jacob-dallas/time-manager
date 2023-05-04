d011cffdd487b04da7848c119b27a7e875c249f0
Pavelka <jacob.pavelka@dallascityhall.com>
2023-05-04 15:56:05 -0500

    deleted too much

--- time_management.psm1
+++ time_management.psm1
@@ -7,3 +7,4 @@ function Initialize-Log {
     $module_path = "C:\Users\jacob.pavelka\Documents\PowerShell\Modules"
     $python_path = $module_path + "\time_management\.venv\Scripts\python.exe"
     Start-Process $python_path "$PSScriptRoot\test.py $local_path\task-log.db" -RedirectStandardOutput "out.txt" -RedirectStandardError "error.txt"
+}
